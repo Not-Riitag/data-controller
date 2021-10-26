9d2085a592009474a1288298598fbf24fb392332
Matthew <matthew.elswick@matthe815.dev>
2021-10-26 15:28:25 -0400

    don't push the config

--- src/UserManager.js
+++ src/UserManager.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 if (!fs.existsSync('config.json'))
     fs.writeFileSync('config.json', JSON.stringify({uri: 'mongodb://localhost:27017/'}))
 
-const client = new MongoClient(JSON.parse(String(fs.readFileSync('config.json'))))
+const client = new MongoClient(JSON.parse(String(fs.readFileSync('config.json'))).uri)
 
 client.connect()
 
