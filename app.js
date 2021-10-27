efe7e715ed59d585bfe5f85477ba9dd3da1a54c8
Matthew <matthew.elswick@matthe815.dev>
2021-10-27 16:15:17 -0400

    credentials within config

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const { createSession } = require('./index').SessionManager
 const { getUserLogin } = require('./index').UserManager
+const config = require('./config.json')
 
 async function run () {
-    const session = await getUserLogin('test', 'test')
+    const session = await getUserLogin(config.username, config.password)
     
     console.log(session)
 }
